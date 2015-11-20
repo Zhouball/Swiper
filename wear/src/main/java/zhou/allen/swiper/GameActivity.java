310caa6198d409c3ae00493aa117e157395eb732
Daniel Hosseinian <daniel.hosseinian@gmail.com>
2015-11-20 00:15:23 -0800

    Updated manifest

--- wear/src/main/java/zhou/allen/swiper/GameActivity.java
+++ wear/src/main/java/zhou/allen/swiper/GameActivity.java
@@ -5,17 +5,16 @@ import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class GameActivity extends Activity {
 
     private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
